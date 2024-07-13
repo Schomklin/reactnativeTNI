0764a999df953cbf8b7fd56bed485de7444a6236
Schomklin <kungchomklin@gmail.com>
2024-07-13 10:22:55 +0700

    ReactNative Fundamental

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StyleSheet, Text, View, Button, Alert } from "react-native";
+import AppFooter from './components/AppFooter';
 
-export default function App() {
+export default function App():React.JSX.Element {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello React Native!</Text>
+      <Button
+        title="Click Me"
+        onPress={() => {
+          Alert.alert("Hi", "React Native is Fun!!");
+        }}
+      />
+      <AppFooter/>     
     </View>
   );
 }
