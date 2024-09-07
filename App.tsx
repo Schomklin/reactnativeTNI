597370b52a0daaca70feec907a68d7ad8669e6f8
Schomklin <kungchomklin@gmail.com>
2024-09-07 08:42:02 +0700

    Update(Sep-07-2024)

--- App.tsx
+++ App.tsx
@@ -1,29 +1,42 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import ProfileScreen from './components/ProfileScreen'
-import UseEffectExampl from './components/UseEffectExampl'
-import FlastListExample from './components/FlastListExample'
-import FlatListcallBackend from './components/FlatListcallBackend'
-import NewsApp from './components/NewsApp'
-import AxiosgetData from './components/AxiosgetData'
-import AxiosPostData from './components/AxiosPostData'
-import WeatherLondon from './components/WeatherLondon'
-import WeatherBangkok from './components/WeatherBangkok'
+import React from "react";
 
+import HomeScreen from "./screens/HomeScreen";
+import AboutScreen from "./screens/AboutScreen";
 
-const App = ():React.JSX.Element => {
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import CreatePostScreen from "./screens/CreatePostScreen";
+
+const App = (): React.JSX.Element => {
+  const HomeStack = createNativeStackNavigator();
+
   return (
-    <View>
-      {/* <ProfileScreen/>       */}
-      {/* <FlastListExample/> */}
-      {/* <FlatListcallBackend/> */}
-      {/* <NewsApp/> */}
-      {/* <AxiosgetData/> */}
-      {/* <AxiosPostData/> */}
-      {/* <WeatherLondon/> */}
-      <WeatherBangkok/>
-    </View>
-  )
-}
+    <NavigationContainer>
+      <HomeStack.Navigator
+        initialRouteName="Home"
+        screenOptions={{    //Global
+          headerStyle: { backgroundColor: "#20b2aa" },
+          headerTintColor: "white",
+          headerTitleStyle: { fontWeight: "bold" },
+        }}
+      >
+        <HomeStack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "หน้าหลัก" }}
+        />
+        <HomeStack.Screen
+          name="About"
+          component={AboutScreen}
+          options={{
+          title:'เกี่ยวกับเรา',
+          headerTitleAlign:'center'
+         }} 
+        />
+        <HomeStack.Screen name="CreatePost" component={CreatePostScreen} />
+      </HomeStack.Navigator>
+    </NavigationContainer>
+  );
+};
 
-export default App
+export default App;
---
+++ components/WeatherApp.tsx
@@ -0,0 +1,114 @@
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Modal } from 'react-native';
+import WeatherLondon from './WeatherLondon';
+import WeatherBangkok from './WeatherBangkok';
+
+const WeatherApp = (): React.JSX.Element => {
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [selectedCity, setSelectedCity] = useState('');
+
+  const toggleModal = (city: string) => {
+    setSelectedCity(city);
+    setModalVisible(true);
+  };
+
+  const renderWeatherComponent = () => {
+    if (selectedCity === 'London') {
+      return <WeatherLondon />;
+    } else if (selectedCity === 'Bangkok') {
+      return <WeatherBangkok />;
+    }
+    return null;
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Weather App</Text>
+      <TouchableOpacity style={styles.button} onPress={() => toggleModal('London')}>
+        <Text style={styles.buttonText}>LONDON</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={() => toggleModal('Bangkok')}>
+        <Text style={styles.buttonText}>BANGKOK</Text>
+      </TouchableOpacity>
+
+      <Modal
+        visible={isModalVisible}
+        transparent={true}
+        animationType="slide"    
+      >
+        <View style={styles.modalBackground}>
+          <View style={styles.modalContainer}>
+            {renderWeatherComponent()}
+            <TouchableOpacity 
+             style={styles.closeButton} 
+             onPress={() => setModalVisible(false)}
+            >
+              <Text style={styles.closeButtonText}>Close</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+    marginTop:70
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  button: {
+    backgroundColor: '#3AA6B9',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  modalBackground: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContainer: {
+    width: '90%',
+    backgroundColor: '#f9f9f9',
+    borderRadius: 20,
+    padding: 20,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 10,
+    elevation: 5,
+  },
+  closeButton: {
+    backgroundColor: '#FF3B30',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginTop: 20,
+    alignItems:'center'
+  },
+  closeButtonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
+
+export default WeatherApp;
+
+
